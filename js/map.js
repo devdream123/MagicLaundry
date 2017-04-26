8620275caa629ebfcb3fa810ac0fae8e96709812
Dao Ong <daoong@Daos-MacBook-Pro.local>
2017-04-27 10:33:28 +1200

    Add info for Contact Us
    -Add info window for map
    -Add address,phone and email

--- js/map.js
+++ js/map.js
@@ -1,12 +1,34 @@
 $(document).ready(function (){
 
-	var uluru = {lat: -36.908760, lng: 174.666970};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
+	var laundryPos = {lat: -33.902196, lng: 151.179325};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+	  	center: laundryPos
     });
+
+
+
+	var contentString = '<div id="content">'+
+	        '<div id="siteNotice">'+
+	        '</div>'+
+	        '<h4 id="infoHeading" class="firstHeading">Laundry Magic</h4>'+
+	        '<div id="bodyContent">'+
+	        '<p><b>Address: </b>514 King Street. Newtown. NSW. Australia 2042' +
+	        '<p><b>Phone: </b>02 9517 3883' +
+	        '</div>'+
+	        '</div>';
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: contentString,
+		maxWidth: 200
+	});
+
+    var marker = new google.maps.Marker({
+		position: laundryPos,
+		map: map,
+		title: 'Laundry Magic'
+    });
+    infoWindow.open(map, marker);
+
 });
